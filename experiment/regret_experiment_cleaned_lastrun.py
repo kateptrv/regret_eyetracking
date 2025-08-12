79d6fcfa25fa09d8f609a00f1daea5baa592305e
Anh-Thu Dang <anhthu28@stanford.edu>
2025-08-12 15:35:14 -0700

    Commit data files

--- experiment/regret_experiment_cleaned_lastrun.py
+++ experiment/regret_experiment_cleaned_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on Mon Aug 11 15:25:04 2025
+    on Tue Aug 12 15:28:17 2025
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
